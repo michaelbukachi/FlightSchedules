385755e30c2b7aaebc227be1415fea1ec2374ad4
Michael Bukachi <michaelbukachi@gmail.com>
2019-07-29 00:30:42 +0300

    Initial commit

---
+++ app/src/main/java/com/michaelbukachi/flightschedules/FlightSchedulesActivity.kt
@@ -0,0 +1,12 @@
+package com.michaelbukachi.flightschedules
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlightSchedulesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flight_schedules)
+    }
+}
